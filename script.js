85368cf19f2ee39ffa0d8499f096b7cf7dfb87b2
suzby <skbichl@gmail.com>
2022-08-16 21:45:08 -0700

    input & console log

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+input into an array
+    listen for button click 
+    push input to userInput array
+output concat
+
+
+*/
+
+
+function tellTheStory(){
+    let inputArray = []
+    let word = document.querySelector("#one").value
+    inputArray = inputArray.push("word")
+
+    console.log(word)
+}
+
+
+
